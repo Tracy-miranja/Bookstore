2d8368b4c6540a0293dbac29cd75dceb38c81b1c
Tracy-miranja <barazatracy44@gmail.com>
2023-04-07 17:31:34 +0300

    Add book author title and id

--- src/pages/Display.js
+++ src/pages/Display.js
@@ -10,12 +10,19 @@ function Display(){
       Title: 'The Hunger Games',
       Author: 'Suzanne-Collins',
     },
+    {
+      id: 2,
+      categories: 'Horror',
+      Title: 'The Hunger Games',
+      Author: 'Suzanne-Collins',
+    },
   ];
     return( 
         <div>
            <ul>
            {bookLists.map((bookList) =>(
             <li key={bookList.id}>
+              <div>
               <div>
               <h1>{bookList.categories}</h1>
               <h2>{bookList.Title}</h2>
@@ -24,6 +31,15 @@ function Display(){
               <button>comments</button>
               <button>Remove</button>
               <button>Edit</button>
+             </div>
+             <div>
+              <div><h1>64% completed</h1></div>
+              <div>
+                <h3>CURRENT CHAPTER</h3>
+                <h2>CHAPTER 17</h2>
+                <button type="submit">UPDATE PROGRESS</button>
+              </div>
+             </div>
              </div>
              </div>
              </li> 
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ function Next() {
     <h1>ADD NEW BOOK</h1>
     <form>
         <input type="text" name="title" placeholder="Book Title" />
-        <input type="text" name="author" placeholder="Author" />
+        <input type="text" name="author" placeholder="Category" />
         <button type="submit">Add Book</button>
       </form>
     </>
